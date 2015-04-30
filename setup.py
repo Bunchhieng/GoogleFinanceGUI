1e8e3e1113e96194fffa1ff53b7bdf14636fe856
Bunchhieng <Bunchhieng@gmail.com>
2015-04-30 14:56:14 -0400

    add PyInstaller

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Google Finance GUI',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT ',
+    author='Bunchhieng',
+    author_email='Bunchhieng@gmail.com',
+    description=''
+)
